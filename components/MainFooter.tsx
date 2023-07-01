8f845d8d4a5101ac5ad5e60f1519234e83904a7e
Neamul01 <neamulhaque2002@gmail.com>
2023-07-01 12:15:23 +0600

    footer added

---
+++ components/MainFooter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeLayout from './Home/Common/HomeLayout'
+
+function MainFooter() {
+  return (
+    <HomeLayout bg="secondary" color="white">
+      <div className={`text-center font-jost h-20 border-t-4`}>MainFooter</div>
+    </HomeLayout>
+  )
+}
+
+export default MainFooter
