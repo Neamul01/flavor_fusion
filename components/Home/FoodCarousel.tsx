37126606f204b32372700ba1fa1b8569fad4368d
Neamul01 <neamulhaque2002@gmail.com>
2023-07-05 23:14:28 +0600

    food carousel updated

--- components/Home/FoodCarousel.tsx
+++ components/Home/FoodCarousel.tsx
@@ -6,8 +6,10 @@ import { Autoplay, Navigation, Pagination } from 'swiper'
 import './foodCarousel.css'
 import Image from 'next/image'
 import HomeLayout from './Common/HomeLayout'
+import { useWindowSize } from '@/hooks/useWindowSize'
 
 function FoodCarousel() {
+  const screen = useWindowSize()
   const foodItems = [
     {
       id: 1,
@@ -62,36 +64,33 @@ function FoodCarousel() {
     <HomeLayout customClass="my-32">
       <Swiper
         loop={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
         speed={2000}
         watchSlidesProgress={true}
-        slidesPerView={4}
+        slidesPerView={screen?.width > 500 ? 4 : 1}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Pagination, Navigation, Autoplay]}
+        modules={[Pagination, Navigation]}
         className="mySwiper-food"
       >
         {foodItems.map((food) => {
           return (
             <SwiperSlide
               key={food.id}
-              className="relative swiper-slide-food px-16"
+              className="relative swiper-slide-food px-6 md:px-16"
             >
-              <div className="food-img absolute left-0">
-                <Image
-                  src={food.img}
-                  alt={food.name}
-                  height={150}
-                  width={150}
-                />
+              <div className="left-3 absolute ">
+                <div className="food-img relative h-28 md:h-36 w-28 md:w-36">
+                  <Image src={food.img} alt={food.name} fill />
+                </div>
               </div>
               <div className="flex flex-col border ml-auto w-full pl-28">
-                <a className="text-2xl text-secondary font-cormorant">
+                <a className="md:text-2xl font-semibold text-xl text-secondary font-cormorant">
                   {food.name}
                 </a>
                 <p className="text-sm max-w-[10rem] text-secondary/50 font-jost">
