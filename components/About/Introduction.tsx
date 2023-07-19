1803f43b0b0e583586715346ee308fa22473b2a6
Neamul01 <neamulhaque2002@gmail.com>
2023-07-19 22:35:16 +0600

    intro img

--- components/About/Introduction.tsx
+++ components/About/Introduction.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import TopHeading from '../Home/Common/TopHeading'
 import H2heading from '../Home/Common/H2heading'
 import HomeLayout from '../Layouts/Layout'
+import Image from 'next/image'
 
 function Introduction() {
   return (
-    <div>
-      <HomeLayout>
-        <div className="flex flex-col">
-          <TopHeading text="Introduction of Flavor Fusion" />
-          <H2heading text="We Are Experienced Restaurant." color="secondary" />
-        </div>
+    <div className="py-20 bg-white">
+      <HomeLayout className="flex flex-col">
+        <TopHeading text="Introduction of Flavor Fusion" />
+        <H2heading text="We Are Experienced Restaurant." color="secondary" />
       </HomeLayout>
+      <div className="max-w-[95rem] mx-auto">
+        <div className="relative h-[34.1875rem] w-[33.125rem]">
+          <Image
+            className="rounded-md overflow-hidden object-contain"
+            src={'/about/intro-left-img.png'}
+            alt="chef"
+            fill
+          />
+        </div>
+      </div>
     </div>
   )
 }
