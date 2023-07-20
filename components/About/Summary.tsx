bbb68bc2cb8591c3749e508158dce7327252246c
Neamul01 <neamulhaque2002@gmail.com>
2023-07-20 13:09:51 +0600

    summery done

--- components/About/Summary.tsx
+++ components/About/Summary.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { LuChefHat } from 'react-icons/lu'
+import { SiCodechef } from 'react-icons/si'
+import { PiBowlFood } from 'react-icons/pi'
+import { BsPeople } from 'react-icons/bs'
+import { LiaAwardSolid } from 'react-icons/lia'
+import styles from './about.module.css'
 
 type SummeryItem = {
   id: number
@@ -13,42 +17,43 @@ const summeryItems: SummeryItem[] = [
     id: 1,
     count: 100,
     text: 'Professional Chef',
-    icon: <LuChefHat className="text-primary" size={'3rem'} />,
+    icon: <SiCodechef className="text-primary" size={'3rem'} />,
   },
   {
     id: 2,
     count: 130,
     text: 'Food Category',
-    icon: <LuChefHat className="text-primary" size={'3rem'} />,
+    icon: <PiBowlFood className="text-primary" size={'3rem'} />,
   },
   {
     id: 3,
     count: '100k',
     text: 'Customer Satisfy',
-    icon: <LuChefHat className="text-primary" size={'3rem'} />,
+    icon: <BsPeople className="text-primary" size={'3rem'} />,
   },
   {
     id: 4,
     count: 120,
     text: 'Award Wining',
-    icon: <LuChefHat className="text-primary" size={'3rem'} />,
+    icon: <LiaAwardSolid className="text-primary" size={'3rem'} />,
   },
 ]
 
 export default function Summary() {
   return (
-    <div className="bg-white pb-3">
+    <div className={`bg-white py-20 ${styles.about_counter}`}>
       <div className="text-secondary container mx-auto grid grid-cols-12 gap-x-4">
-        {[1, 2, 3, 4].map((item) => (
+        {summeryItems.map((item) => (
           <div
-            key={item}
-            className="col-span-3 px-6 py-4 flex items-center gap-2 border border-secondary/10 rounded-xl"
+            key={item.id}
+            className={`${styles.counter_single} col-span-3 px-6 py-4 flex items-center gap-2 border border-secondary/10 rounded-xl`}
           >
-            <LuChefHat className="text-primary" size={'3rem'} />
+            {/* <LuChefHat className="text-primary" size={'3rem'} /> */}
+            {item.icon}
             <div className="flex flex-col">
-              <p className="text-3xl font-semibold">100</p>
+              <p className="text-3xl font-semibold">{item.count}</p>
               <p className="text-secondary/50 text-xl font-cormorant">
-                Professional Chef
+                {item.text}
               </p>
             </div>
           </div>
