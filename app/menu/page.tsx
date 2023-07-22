17f4b492ce6dd388b8bafa9349479efde77f0b94
Neamul01 <neamulhaque2002@gmail.com>
2023-07-22 18:41:23 +0600

    fooditem component created

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,8 +2,7 @@
 
 import React from 'react'
 import StyleHeading from '@/components/Common/StyleHeading'
-import Layout from '@/components/Layouts/Layout'
-import FoodCard from '@/components/Common/FoodCard'
+import FoodItems from '@/components/Menu/FoodItems'
 
 const breadcrumbsItems = [
   { title: 'Home', href: '/' },
@@ -14,70 +13,7 @@ export default function page() {
   return (
     <>
       <StyleHeading breadcrumbsItems={breadcrumbsItems} text="Menu List" />
-      <Layout bg="white" className="py-20 grid grid-cols-12">
-        {menuItems.map((item) => (
-          <div className="col-span-4" key={item.id}>
-            <FoodCard item={item} />
-          </div>
-        ))}
-      </Layout>
+      <FoodItems />
     </>
   )
 }
-
-const menuItems = [
-  {
-    id: 1,
-    name: 'Full Chicken',
-    description:
-      'It is a long established fact that a reader will be distracted.',
-    img: '/MenuItem/food-item-1.png',
-    price: '45',
-    category: 'Lanch',
-  },
-  {
-    id: 2,
-    name: ' Chicken',
-    description:
-      'It is a long established fact that a reader will be distracted.',
-    img: '/MenuItem/food-item-2.png',
-    price: '40',
-    category: 'Lanch',
-  },
-  {
-    id: 3,
-    name: 'Burger',
-    description:
-      'It is a long established fact that a reader will be distracted.',
-    img: '/MenuItem/food-item-3.png',
-    price: '50',
-    category: 'Lanch',
-  },
-  {
-    id: 4,
-    name: 'Carry',
-    description:
-      'It is a long established fact that a reader will be distracted.',
-    img: '/MenuItem/food-item-4.png',
-    price: '35',
-    category: 'Lanch',
-  },
-  {
-    id: 5,
-    name: 'Full Chicken',
-    description:
-      'It is a long established fact that a reader will be distracted.',
-    img: '/MenuItem/food-item-5.png',
-    price: '30',
-    category: 'Lanch',
-  },
-  {
-    id: 6,
-    name: 'Full Rice',
-    description:
-      'It is a long established fact that a reader will be distracted.',
-    img: '/MenuItem/food-item-6.png',
-    price: '55',
-    category: 'Lanch',
-  },
-]
