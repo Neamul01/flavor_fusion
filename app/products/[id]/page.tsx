dac3f793b17c78b3fd5614ccd0499e689d2f882d
Neamul01 <neamulhaque2002@gmail.com>
2023-07-28 23:42:37 +0600

    swiper added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -3,6 +3,7 @@
 import Heading from '@/components/Common/StyleHeading'
 import Layout from '@/components/Layouts/Layout'
 import Product from '@/components/Products/Product'
+import ProductImage from '@/components/Products/ProductImage'
 import React from 'react'
 
 const breadcrumbsItems = [
@@ -23,7 +24,14 @@ export default function page({ params }: { params: { id: string } }) {
     <div>
       <Heading text="Single page" breadcrumbsItems={breadcrumbsItems} />
       <Layout bg="white" color="secondary">
-        <Product params={params.id} />
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">
+            <ProductImage />
+          </div>
+          <div className="col-span-6">
+            <Product params={params.id} />
+          </div>
+        </div>
       </Layout>
     </div>
   )
