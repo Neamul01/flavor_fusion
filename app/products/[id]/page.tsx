bfbe053e3477ce5da8f634370a2872815e53d2fa
Neamul01 <neamulhaque2002@gmail.com>
2023-07-29 03:12:12 +0600

    tab added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,6 +4,7 @@ import Heading from '@/components/Common/StyleHeading'
 import Layout from '@/components/Layouts/Layout'
 import Product from '@/components/Products/Product'
 import ProductImage from '@/components/Products/ProductImage'
+import { Tabs } from '@mantine/core'
 import React from 'react'
 
 const breadcrumbsItems = [
@@ -31,6 +32,27 @@ export default function page({ params }: { params: { id: string } }) {
           <div className="col-span-6 px-4">
             <Product params={params.id} />
           </div>
+        </div>
+        <div className="my-10">
+          <h2 className="text-3xl md:text-5xl font-semibold">Item Details</h2>
+          <Tabs defaultValue="gallery" className="flex gap-4 my-10">
+            <Tabs.List className="flex flex-col gap-3">
+              <Tabs.Tab value="gallery" className="bg-secondary">
+                <div className="text-xl text-white">Gallery</div>
+              </Tabs.Tab>
+              <Tabs.Tab value="messages" className="bg-secondary">
+                <div className="text-xl text-white">Messages</div>
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="gallery" pt="xs">
+              Gallery tab content
+            </Tabs.Panel>
+
+            <Tabs.Panel value="messages" pt="xs">
+              Messages tab content
+            </Tabs.Panel>
+          </Tabs>
         </div>
       </Layout>
     </div>
