22f6365427314058d3564a46b5156a64e637590c
Neamul01 <neamulhaque2002@gmail.com>
2023-07-29 23:44:24 +0600

    card done

--- components/Products/ProductReview.tsx
+++ components/Products/ProductReview.tsx
@@ -5,8 +5,10 @@ export default function ProductReview() {
   return (
     <div className="w-full">
       <h2 className="text-3xl font-semibold mb-2">Comments(02) :</h2>
-      <div className="flex flex-col gap-3">
-        <ReviewCard />
+      <div className="flex flex-col gap-4">
+        {[1, 2].map((item) => (
+          <ReviewCard key={item} />
+        ))}
       </div>
     </div>
   )
--- components/Products/ReviewCard/ReviewCard.tsx
+++ components/Products/ReviewCard/ReviewCard.tsx
@@ -4,15 +4,23 @@ import React from 'react'
 
 export default function ReviewCard() {
   return (
-    <div className="bg-primary/10 px-5 py-7">
-      <div className="flex gap-6">
-        <div className="relative w-[78px] h-[77px] ">
+    <div className="bg-primary/5 px-5 py-7">
+      <div className="flex gap-6 items-center">
+        <div className="relative w-[78px] h-[77px] flex-shrink-0 rounded-lg overflow-hidden">
           <Image alt="comment" src={'/single-item/comment-img-1.png'} fill />
         </div>
-        <div className="flex gap-3 flex-col">
-          <div className="flex justify-between flex-col">
-            <p className="text-semibold text-xl">John wick,</p>
-            <Rating value={5} size="xs" />
+        <div className="flex gap-3 flex-col mr-4">
+          <div className="flex justify-between">
+            <div className="flex gap-4">
+              <div className="flex flex-col">
+                <p className="text-semibold text-xl">John wick,</p>
+                <Rating value={5} size="xs" />
+              </div>
+              <p className=" text-secondary/70">06 july,2022</p>
+            </div>
+            <p className="text-primary text-lg cursor-pointer hover:text-secondary/90 transition-transform">
+              Replay
+            </p>
           </div>
           <p className="text-secondary/70 text-lg">
             I must explain to you how all this mistaken idea of denouncing
