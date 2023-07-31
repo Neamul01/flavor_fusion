d567d72172f89dbc30866d8ab47ba2c3b77fd7cf
Neamul01 <neamulhaque2002@gmail.com>
2023-08-01 02:57:44 +0600

    card data created

--- components/Categories/TabItem/TabItem.tsx
+++ components/Categories/TabItem/TabItem.tsx
@@ -25,3 +25,48 @@ export default function TabItem({ tabName }: { tabName: string }) {
     </div>
   )
 }
+
+const tabContent = [
+  {
+    id: 1,
+    title: 'Mixed Vegetable Salad',
+    description: 'Complete account of the system.',
+    price: 12.99,
+    img: '/categories/fast-food11.png',
+  },
+  {
+    id: 2,
+    title: 'Greek Salad',
+    description: 'Complete account of the system.',
+    price: 12.99,
+    img: '/categories/fast-food12.png',
+  },
+  {
+    id: 3,
+    title: 'Summer Corn',
+    description: 'Complete account of the system.',
+    price: 12.99,
+    img: '/categories/fast-food13.png',
+  },
+  {
+    id: 4,
+    title: 'Taco Salad Item',
+    description: 'Complete account of the system.',
+    price: 12.99,
+    img: '/categories/fast-food14.png',
+  },
+  {
+    id: 5,
+    title: 'Naga Chicken',
+    description: 'Complete account of the system.',
+    price: 12.99,
+    img: '/categories/fast-food15.png',
+  },
+  {
+    id: 6,
+    title: 'Broccoli Pasta Item',
+    description: 'Complete account of the system.',
+    price: 12.99,
+    img: '/categories/fast-food16.png',
+  },
+]
