7c284d915b23e2a181269b598a545b20e81c04b1
Neamul01 <neamulhaque2002@gmail.com>
2023-08-02 04:38:12 +0600

    getintouch responsived

--- components/Contact/GetInTouch.tsx
+++ components/Contact/GetInTouch.tsx
@@ -20,17 +20,17 @@ export default function GetInTouch() {
 
   return (
     <Layout className="py-20">
-      <div className="contact_form relative bg-[url('/contact/reservation-1-bg.png')] w-full bg-no-repeat bg-cover rounded-lg p-20">
+      <div className="contact_form relative bg-[url('/contact/reservation-1-bg.png')] w-full bg-no-repeat bg-cover rounded-lg px-6 py-12 md:px-0 md:py-0 md:p-20">
         <div className="flex flex-col items-center justify-center z-20">
           <TopHeading text="Contact Us" />
           <H2heading color="white" text="Get In Touch" />
         </div>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col min-w-fit items-center justify-center">
           <form
             onSubmit={form.onSubmit((values) => console.log(values))}
-            className="w-full px-16 mt-16 flex flex-col gap-8"
+            className="w-full md:px-16 mt-16 flex flex-col gap-8"
           >
-            <div className="flex gap-4 relative">
+            <div className="flex flex-col md:flex-row gap-4 relative">
               <MantineTextInput
                 formProps={{ ...form.getInputProps('email') }}
                 placeholder="Email"
