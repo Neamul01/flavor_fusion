6b4a8343ff1a827417dc784dcd6ad21eb22f17f2
Neamul01 <neamulhaque2002@gmail.com>
2023-08-03 20:22:02 +0600

    settings added

---
+++ app/admin/settings/page.tsx
@@ -0,0 +1,18 @@
+import CardProfile from '@/components/Admin/CardProfile'
+import CardSettings from '@/components/Admin/CardSettings'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <div className="flex flex-wrap bg-white">
+        <div className="w-full lg:w-8/12 px-4">
+          <CardSettings />
+        </div>
+        <div className="w-full lg:w-4/12 px-4">
+          <CardProfile />
+        </div>
+      </div>
+    </>
+  )
+}
