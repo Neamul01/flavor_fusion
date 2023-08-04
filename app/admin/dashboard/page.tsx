dade424abe585ef31e09ae7fdc93b16b83984fd4
Neamul01 <neamulhaque2002@gmail.com>
2023-08-04 18:08:09 +0600

    new chart line added

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -7,6 +7,7 @@ import CardBarChart from '@/components/Admin/Cards/CardBarChart'
 import CardPageVisits from '@/components/Admin/Cards/CardPageVisits'
 import CardSocialTraffic from '@/components/Admin/Cards/CardSocialTraffic'
 import Admin from '@/components/Layouts/Admin'
+import CardLineChart2 from '@/components/Admin/Cards/CardLineChart2'
 
 // layout for page
 
@@ -15,7 +16,7 @@ export default function Dashboard() {
     <Admin>
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardLineChart />
+          <CardLineChart2 />
         </div>
         <div className="w-full xl:w-4/12 px-4">{<CardBarChart />}</div>
       </div>
---
+++ components/Admin/Cards/CardLineChart2.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+}
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+const rawData = ['65, 78, 66, 44, 56, 67, 75']
+const chartData = rawData.map((data, index) => ({ x: labels[index], y: data }))
+console.log('chartData', chartData)
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 2',
+      data: rawData.map((data, index) => ({ x: labels[index], y: data })),
+      //   data: "65, 78, 66, 44, 56, 67, 75",
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [
+        { x: 'January', y: 20 },
+        { x: 'February', y: 10 },
+      ],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+}
+
+export default function CardLineChart2() {
+  return <Line options={options} data={data} />
+}
