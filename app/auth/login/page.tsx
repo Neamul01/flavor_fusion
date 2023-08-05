0865f43ef8db2d33c9d1d993a3c9323d7ed02ba3
Neamul01 <neamulhaque2002@gmail.com>
2023-08-05 20:38:07 +0600

    login register warning solved

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Admin from '@/components/Layouts/Admin'
+import Image from 'next/image'
 
 // layout for page
 
@@ -10,7 +11,7 @@ export default function Login() {
       <div className="container mx-auto px-4 h-full md:mt-20">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-xl shadow-primary rounded-lg bg-blueGray-200 border-0 bg-white">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg shadow-primary rounded-lg border-0 bg-white">
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
@@ -19,17 +20,21 @@ export default function Login() {
                 </div>
                 <div className="btn-wrapper text-center">
                   <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    className="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
                     type="button"
                   >
-                    <img alt="..." className="w-5 mr-1" src="/img/github.svg" />
+                    <div className="w-5 h-5 mr-1 relative">
+                      <Image src="/img/github.svg" fill alt="..." />
+                    </div>
                     Github
                   </button>
                   <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    className="bg-white active:bg-blueGray-50 text-blueGray-700 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
                     type="button"
                   >
-                    <img alt="..." className="w-5 mr-1" src="/img/google.svg" />
+                    <div className="w-5 h-5 mr-1 relative">
+                      <Image src="/img/google.svg" fill alt="..." />
+                    </div>
                     Google
                   </button>
                 </div>
