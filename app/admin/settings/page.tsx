1e87a30fb83bffaad43110df3b826eba741f5a3c
Neamul01 <neamulhaque2002@gmail.com>
2023-08-07 00:23:51 +0600

    dashboard layout created

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,19 +1,16 @@
 import CardProfile from '@/components/Admin/CardProfile'
 import CardSettings from '@/components/Admin/CardSettings'
-import Admin from '@/components/Layouts/Admin'
 import React from 'react'
 
 export default function page() {
   return (
-    <Admin>
-      <div className="flex flex-wrap bg-white">
-        <div className="w-full lg:w-8/12 px-4">
-          <CardSettings />
-        </div>
-        <div className="w-full lg:w-4/12 px-4">
-          <CardProfile />
-        </div>
+    <div className="flex flex-wrap bg-white">
+      <div className="w-full lg:w-8/12 px-4">
+        <CardSettings />
       </div>
-    </Admin>
+      <div className="w-full lg:w-4/12 px-4">
+        <CardProfile />
+      </div>
+    </div>
   )
 }
